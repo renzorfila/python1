7950cf4ea33c209a35cbb8288f34f7f2845370e9
renzorfila <renzorfila@hotmail.com>
2024-09-19 05:57:39 -0300

    atualização de python

--- exercicio8.py
+++ exercicio8.py
@@ -1,6 +1,6 @@
 print ("Renzo Trigo Orfila\n1051392421006\n")
 
-celsius= 24
+celsius= float(input("Temperatura atual em Celsius:"))
 fahren= float(celsius*1.8)+32
 
-print(f"Está 24 graus Celsius hoje. Em Fahrenheit, é equivalente a: {fahren}")
+print(f"Está {celsius} graus Celsius atualmente. Em Fahrenheit, é equivalente a: {fahren}")
