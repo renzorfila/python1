1145ef1dc0e80e1813d8611e18defbd7ca1c605d
renzorfila <renzorfila@hotmail.com>
2024-09-19 09:16:54 -0300

    mais uma atualização

--- exercicio10.py
+++ exercicio10.py
@@ -1,1 +1,13 @@
 print ("Renzo Trigo Orfila\n1051392421006\n")
+
+placadevideo= float(input("Valor da placa de vídeo: R$"))
+processador= float(input("Valor do processador: R$"))
+placamae= float(input("Valor da placa-mãe: R$"))
+ram= float(input("Valor da memória RAM: R$"))
+fonte= float(input("Valor da fonte: R$"))
+total= (placadevideo)+(processador)+(placamae)+(ram)+(fonte)
+
+print(f"Total a ser pago: {total}")
+pagamento= float(input("Total a pagar: R$"))
+
+print(f"O troco recebido pelo pagamento é de: R$ {pagamento-total}")
--- exercicio9.py
+++ exercicio9.py
@@ -1,1 +1,7 @@
 print ("Renzo Trigo Orfila\n1051392421006\n")
+
+dolar= float(input("Cotação do dólar atualmente: U$"))
+dolardesejado= float(input("Valor do dólar desejado: U$"))
+conversao= float(dolardesejado*5.48)
+
+print(f"Em reais, a cotação do dólar é de: R${int(conversao)}")
